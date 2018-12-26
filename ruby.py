898c2f56de36ce8220039013e42527a1bfe79258
Snaacky <snaacky@gmail.com>
2018-12-26 16:02:31 -0500

    Improved hopper, fixed issue that broke code, added little attempt at future-proofing
    Pymem updated and I didn't think to leave a version in requirements.txt.

--- ruby.py
+++ ruby.py
@@ -3,12 +3,12 @@ import pymem
 import pymem.process
 import time
 
-dwForceJump = (0x50DE048)
-dwLocalPlayer = (0xC5E87C)
-m_fFlags = (0x100)
+dwForceJump = (0x5170DB0)
+dwLocalPlayer = (0xCBD6B4)
+m_fFlags = (0x104)
 
 pm = pymem.Pymem("csgo.exe")
-client = pymem.process.module_from_name(pm.process_id, "client_panorama.dll").base_address
+client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 
 
 def main():
@@ -18,14 +18,13 @@ def main():
         try:
             player = pm.read_int(client + dwLocalPlayer)
             force_jump = client + dwForceJump
-            on_ground = pm.read_char(player + m_fFlags)
-
+            on_ground = pm.read_int(player + m_fFlags)
+            
             if keyboard.is_pressed("space"):
-                if on_ground:
+                if on_ground == 257:
                     pm.write_int(force_jump, 5)
-                    time.sleep(0.20)
+                    time.sleep(0.17)
                     pm.write_int(force_jump, 4)
-                    time.sleep(0.20)
             time.sleep(0.002)
         except pymem.exception.MemoryReadError:
             pass
