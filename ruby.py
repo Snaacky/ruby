4a2257e38ef9976481094293468837fd49bea159
Snaacky <Snaacky@pm.me>
2020-04-20 08:23:13 -0400

    feat: 🎸 Fixed CPU usage, fixed hopping while ALT-TAB'd, etc

--- ruby.py
+++ ruby.py
@@ -2,32 +2,32 @@ import keyboard
 import pymem
 import pymem.process
 import time
+from win32gui import GetWindowText, GetForegroundWindow
 
-dwForceJump = (0x5170DB0)
-dwLocalPlayer = (0xCBD6B4)
+dwClientState = (0x589DCC)
+dwForceJump = (0x51ED750)
+dwLocalPlayer = (0xD2FB84)
 m_fFlags = (0x104)
-
-pm = pymem.Pymem("csgo.exe")
-client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 
 
 def main():
     print("Ruby has launched.")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
 
     while True:
-        try:
-            player = pm.read_int(client + dwLocalPlayer)
-            force_jump = client + dwForceJump
-            on_ground = pm.read_int(player + m_fFlags)
-            
+        if GetWindowText(GetForegroundWindow()) == "Counter-Strike: Global Offensive":
             if keyboard.is_pressed("space"):
-                if on_ground == 257:
-                    pm.write_int(force_jump, 5)
-                    time.sleep(0.17)
-                    pm.write_int(force_jump, 4)
-            time.sleep(0.002)
-        except pymem.exception.MemoryReadError:
-            pass
+                force_jump = client + dwForceJump
+                player = pm.read_int(client + dwLocalPlayer)
+                if player:
+                    on_ground = pm.read_int(player + m_fFlags)
+                    if on_ground:
+                        if on_ground == 257:
+                            pm.write_int(force_jump, 5)
+                            time.sleep(0.17)
+                            pm.write_int(force_jump, 4)
+        time.sleep(0.001)
 
 
 if __name__ == '__main__':
